a2d68b2bcbace6ec8e864913770fcda39e70f0aa
Paul T <4277512+pault-tech@users.noreply.github.com>
2023-09-22 12:52:14 -1000

    testing github.dev

--- a_jupyter_spacemacs.sh
+++ a_jupyter_spacemacs.sh
@@ -1,5 +1,6 @@
 #NOTE: prefered method in spacemacs is to use python mode (dont enable ipython-notebook) and install jupytext
 
+
 #NOTE:a running jupyter server is required for spacemacs ipython-notebook
 #for jupytext conversion to work in spacemacs ipython-notebook layer need to
 #Have an IPython notebook running and jupytext installed (for conversion)
